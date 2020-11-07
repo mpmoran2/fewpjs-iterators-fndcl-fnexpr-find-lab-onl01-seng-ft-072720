87fd41fb2d8e0f1d12401241482ddc732e2a7a3f
mpmoran <kamokumoran@gmail.com>
2020-11-06 17:19:46 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(record) {
+let win = record.find( function(w) { return w.result === "W"})
+return win ? win.year : undefined
+};
